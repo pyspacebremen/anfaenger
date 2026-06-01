9e5229f7afb5e6c4a4bad31fb6ee3eb273c32e1e
pySpaceBremen <148010240+pyspacebremen@users.noreply.github.com>
2026-06-01 20:52:33 +0000

    create 03_listen.py

---
+++ 03_listen.py
@@ -0,0 +1,8 @@
+# Tupel
+# Ein Tupel ist eine unveränderbare Sequenz.
+# Ein Tupel kann aus Items mit beliebigen Datentypen zusammen gesetzt sein.
+# Tupel werden in runden Klammern gesetzt und durch Komma getrent.
+
+
+t = (1, 2, 3)
+print(t)
