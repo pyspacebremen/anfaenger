e05ab4be7702d754d482f01c67e400a011eefa40
pySpaceBremen <148010240+pyspacebremen@users.noreply.github.com>
2026-06-01 22:36:30 +0200

    Create 02_variablen.py with variable examples
    Add example of dynamic typing in Python variables.

---
+++ 02_variablen.py
@@ -0,0 +1,9 @@
+# Variablen sind in Python eine dynamisch Typisiert.
+# Das heißt der Inhalt und der Typ kann sich während der Laufzeit des Programms jederzeit ändern.
+
+x = "Hallo Welt!"
+print(x)
+
+x = 42
+print(x)
+
